e73bfc2b0910e82e67aa44776716ee8f133e7ef4
Jase Chen <xxmm@live.cn>
2018-06-09 02:07:14 +0800

    界面调整

--- basewin.py
+++ basewin.py
@@ -17,9 +17,9 @@ import wx.xrc
 class baseMainWindow ( wx.Frame ):
 	
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"JCDown", pos = wx.DefaultPosition, size = wx.Size( 582,415 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"JCDown", pos = wx.DefaultPosition, size = wx.Size( 582,450 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 		
-		self.SetSizeHints( wx.Size( 582,415 ), wx.Size( 582,415 ) )
+		self.SetSizeHints( wx.Size( 582,450 ), wx.Size( 582,450 ) )
 		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
 		
 		self.menubar = wx.MenuBar( 0 )
@@ -49,6 +49,44 @@ class baseMainWindow ( wx.Frame ):
 		self.m_notebook2.SetForegroundColour( wx.Colour( 255, 255, 255 ) )
 		self.m_notebook2.SetBackgroundColour( wx.Colour( 255, 255, 255 ) )
 		
+		self.video_panel = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer101 = wx.BoxSizer( wx.VERTICAL )
+		
+		bSizer41 = wx.BoxSizer( wx.VERTICAL )
+		
+		self.urls_lable_staticText1 = wx.StaticText( self.video_panel, wx.ID_ANY, u"输入网址", wx.Point( -1,-1 ), wx.DefaultSize, 0 )
+		self.urls_lable_staticText1.Wrap( -1 )
+		bSizer41.Add( self.urls_lable_staticText1, 0, wx.LEFT, 5 )
+		
+		
+		bSizer41.Add( ( 0, 0), 0, wx.BOTTOM|wx.TOP, 2 )
+		
+		gSizer1 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		self.video_url_textCtrl = wx.TextCtrl( self.video_panel, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 430,-1 ), 0 )
+		gSizer1.Add( self.video_url_textCtrl, 0, wx.ALL, 5 )
+		
+		self.fetch_button = wx.Button( self.video_panel, wx.ID_ANY, u"获取", wx.Point( -1,-1 ), wx.DefaultSize, 0 )
+		gSizer1.Add( self.fetch_button, 0, wx.ALIGN_RIGHT|wx.ALL, 5 )
+		
+		
+		bSizer41.Add( gSizer1, 1, wx.EXPAND, 5 )
+		
+		Stream_listBoxChoices = []
+		self.Stream_listBox = wx.ListBox( self.video_panel, wx.ID_ANY, wx.DefaultPosition, wx.Size( 430,150 ), Stream_listBoxChoices, 0 )
+		bSizer41.Add( self.Stream_listBox, 0, wx.ALL, 5 )
+		
+		
+		bSizer101.Add( bSizer41, 1, wx.EXPAND, 5 )
+		
+		self.m_staticline11 = wx.StaticLine( self.video_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+		bSizer101.Add( self.m_staticline11, 0, wx.EXPAND|wx.TOP, 10 )
+		
+		
+		self.video_panel.SetSizer( bSizer101 )
+		self.video_panel.Layout()
+		bSizer101.Fit( self.video_panel )
+		self.m_notebook2.AddPage( self.video_panel, u"视频", False )
 		self.forum_image_panel = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		self.forum_image_panel.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
 		
@@ -72,49 +110,78 @@ class baseMainWindow ( wx.Frame ):
 		self.m_staticline1 = wx.StaticLine( self.forum_image_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
 		bSizer10.Add( self.m_staticline1, 0, wx.EXPAND|wx.TOP, 10 )
 		
-		gbSizer2 = wx.GridBagSizer( 0, 0 )
-		gbSizer2.SetFlexibleDirection( wx.BOTH )
-		gbSizer2.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-		
-		self.location_lable_staticText = wx.StaticText( self.forum_image_panel, wx.ID_ANY, u"保存位置：", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.location_lable_staticText.Wrap( -1 )
-		gbSizer2.Add( self.location_lable_staticText, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.LEFT|wx.TOP, 24 )
-		
-		self.image_local_dirPicker = wx.DirPickerCtrl( self.forum_image_panel, wx.ID_ANY, wx.EmptyString, u"Select a folder", wx.DefaultPosition, wx.Size( 420,-1 ), wx.DIRP_DEFAULT_STYLE )
-		self.image_local_dirPicker.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
-		
-		gbSizer2.Add( self.image_local_dirPicker, wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 1 ), wx.RIGHT|wx.TOP, 18 )
-		
-		
-		bSizer10.Add( gbSizer2, 0, wx.SHAPED, 5 )
-		
-		gbSizer4 = wx.GridBagSizer( 0, 0 )
-		gbSizer4.SetFlexibleDirection( wx.BOTH )
-		gbSizer4.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
-		
-		gbSizer4.SetMinSize( wx.Size( 530,-1 ) ) 
-		self.image_download_button = wx.Button( self.forum_image_panel, wx.ID_ANY, u"开始下载", wx.Point( -1,-1 ), wx.Size( -1,-1 ), 0 )
-		gbSizer4.Add( self.image_download_button, wx.GBPosition( 1, 2 ), wx.GBSpan( 1, 1 ), wx.LEFT, 73 )
-		
-		self.image_save_individual_checkBox = wx.CheckBox( self.forum_image_panel, wx.ID_ANY, u"每个页面的图片保存到对应的子文件夹中", wx.DefaultPosition, wx.DefaultSize, 0 )
-		gbSizer4.Add( self.image_save_individual_checkBox, wx.GBPosition( 1, 1 ), wx.GBSpan( 1, 1 ), wx.EXPAND|wx.LEFT, 87 )
-		
-		
-		bSizer10.Add( gbSizer4, 1, 0, 0 )
-		
 		
 		self.forum_image_panel.SetSizer( bSizer10 )
 		self.forum_image_panel.Layout()
 		bSizer10.Fit( self.forum_image_panel )
 		self.m_notebook2.AddPage( self.forum_image_panel, u"论坛图片", False )
-		self.video_panel = wx.Panel( self.m_notebook2, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		self.m_notebook2.AddPage( self.video_panel, u"视频", False )
 		
 		bSizer1.Add( self.m_notebook2, 1, wx.EXPAND |wx.ALL, 5 )
+		
+		bSizer9 = wx.BoxSizer( wx.VERTICAL )
+		
+		gbSizer2 = wx.GridBagSizer( 0, 0 )
+		gbSizer2.SetFlexibleDirection( wx.BOTH )
+		gbSizer2.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		self.location_lable_staticText = wx.StaticText( self, wx.ID_ANY, u"保存位置：", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.location_lable_staticText.Wrap( -1 )
+		gbSizer2.Add( self.location_lable_staticText, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.LEFT|wx.TOP, 10 )
+		
+		self.save_local_dirPicker = wx.DirPickerCtrl( self, wx.ID_ANY, wx.EmptyString, u"Select a folder", wx.DefaultPosition, wx.Size( 465,-1 ), wx.DIRP_DEFAULT_STYLE )
+		self.save_local_dirPicker.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOW ) )
+		
+		gbSizer2.Add( self.save_local_dirPicker, wx.GBPosition( 0, 1 ), wx.GBSpan( 1, 1 ), wx.ALL, 10 )
+		
+		
+		bSizer9.Add( gbSizer2, 0, wx.SHAPED, 5 )
+		
+		gSizer4 = wx.GridSizer( 0, 2, 0, 0 )
+		
+		bSizer22 = wx.BoxSizer( wx.VERTICAL )
+		
+		fgSizer4 = wx.FlexGridSizer( 0, 0, 0, 0 )
+		fgSizer4.SetFlexibleDirection( wx.BOTH )
+		fgSizer4.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		fgSizer4.SetMinSize( wx.Size( 560,-1 ) ) 
+		self.save_individual_checkBox = wx.CheckBox( self, wx.ID_ANY, u"保存到对应子文件夹中", wx.DefaultPosition, wx.Size( -1,-1 ), 0 )
+		fgSizer4.Add( self.save_individual_checkBox, 0, wx.ALL, 10 )
+		
+		fgSizer1 = wx.FlexGridSizer( 1, 0, 0, 0 )
+		fgSizer1.SetFlexibleDirection( wx.BOTH )
+		fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		self.proxy_checkBox = wx.CheckBox( self, wx.ID_ANY, u"Proxy:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.proxy_checkBox.SetForegroundColour( wx.Colour( 0, 0, 0 ) )
+		
+		fgSizer1.Add( self.proxy_checkBox, 0, wx.ALL, 10 )
+		
+		self.m_textCtrl9 = wx.TextCtrl( self, wx.ID_ANY, u"socks5://127.0.0.1:1086/", wx.Point( -1,-1 ), wx.Size( 180,-1 ), 0 )
+		fgSizer1.Add( self.m_textCtrl9, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
+		
+		
+		fgSizer4.Add( fgSizer1, 1, wx.EXPAND, 5 )
+		
+		self.download_button = wx.Button( self, wx.ID_ANY, u"开始下载", wx.Point( -1,-1 ), wx.DefaultSize, 0 )
+		fgSizer4.Add( self.download_button, 0, wx.ALIGN_CENTER_VERTICAL|wx.LEFT, 44 )
+		
+		
+		bSizer22.Add( fgSizer4, 1, wx.EXPAND, 5 )
+		
+		
+		gSizer4.Add( bSizer22, 1, 0, 0 )
+		
+		
+		bSizer9.Add( gSizer4, 1, wx.EXPAND, 5 )
+		
+		
+		bSizer1.Add( bSizer9, 1, wx.EXPAND, 5 )
 		
 		
 		self.SetSizer( bSizer1 )
 		self.Layout()
+		self.statusBar = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
 		
 		self.Centre( wx.BOTH )
 		
@@ -122,7 +189,7 @@ class baseMainWindow ( wx.Frame ):
 		self.Bind( wx.EVT_MENU, self.exit_menuItemOnMenuSelection, id = self.exit_menuItem.GetId() )
 		self.Bind( wx.EVT_MENU, self.rule_menuItemOnMenuSelection, id = self.rule_menuItem.GetId() )
 		self.Bind( wx.EVT_MENU, self.about_menuItemOnMenuSelection, id = self.about_menuItem.GetId() )
-		self.image_download_button.Bind( wx.EVT_BUTTON, self.image_download_buttonOnButtonClick )
+		self.fetch_button.Bind( wx.EVT_BUTTON, self.fetch_buttonOnButtonClick )
 	
 	def __del__( self ):
 		pass
@@ -138,7 +205,7 @@ class baseMainWindow ( wx.Frame ):
 	def about_menuItemOnMenuSelection( self, event ):
 		event.Skip()
 	
-	def image_download_buttonOnButtonClick( self, event ):
+	def fetch_buttonOnButtonClick( self, event ):
 		event.Skip()
 	
 
@@ -153,6 +220,16 @@ class rule_Dialog ( wx.Dialog ):
 		
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 		
+		fgSizer3 = wx.FlexGridSizer( 0, 2, 0, 0 )
+		fgSizer3.SetFlexibleDirection( wx.BOTH )
+		fgSizer3.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		self.m_checkBox9 = wx.CheckBox( self, wx.ID_ANY, u"保存使用状态", wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer3.Add( self.m_checkBox9, 0, wx.ALL, 5 )
+		
+		
+		self.SetSizer( fgSizer3 )
+		self.Layout()
 		
 		self.Centre( wx.BOTH )
 	
